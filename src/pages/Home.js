1cf89efb2a6d16525cea632260cefebd42dbd971
Sam2303 <sjp2000@live.com>
2021-09-25 21:39:12 +0100

    initial start on implemeting the connection to the airtable DB .
    fetches all the projects from the DB and list them on the page, need to make if statement so button does not show if live site is not avaiable

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Airtable from "airtable";
 import profilePic from '../resources/profile.jpg'
 
 const Home = () => {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+  
+      async function fetchProjects() {
+        //   const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY }).base(
+        //       process.env.AIRTABLE_BASE_ID
+        //   );
+        var base = new Airtable({apiKey: 'keyqTF0MYSk8YUp0m'}).base('appLl8YFIJRXggdCw');
+          const table = base("Projects");
+          try {
+            setProjects( await table.select({}).all());
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchProjects()
+    }, []);
+    console.log(projects);
+
     return (
         <div className="homeContainer">
             <div className="welcomeMsg">
@@ -9,8 +31,23 @@ const Home = () => {
                 <p>Graduate Computer Scientist</p>
                 <p>Solution-oriented Software Developer, with a high interest in Web Technologies</p>
                 <img src={profilePic} alt="Sam Parsons in there graduation Grown"></img>
+            </div> 
+
+            <div className="projects">
+                {projects.map((project) => {
+                    return (
+                        <div>
+                            <h3>{project.fields.Name}</h3>
+                            <p>{project.fields.Description}</p>
+                            <a href={project.fields.Code}>Code</a>
+                                                       
+                        </div>
+                    )
+                })}
             </div>
         </div>
+
+       
     )
 }
 

b5db5b3699cde531d1d178ad58ac11d7504a8d32
Sam2303 <sjp2000@live.com>
2021-09-26 11:10:43 +0100

    projects

---
+++ src/components/Projects.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Airtable from "airtable";
+
+const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      var base = new Airtable({
+        apiKey: process.env.REACT_APP_AIRTABLE_API_KEY,
+      }).base(process.env.REACT_APP_AIRTABLE_BASE_ID);
+      const table = base("Projects");
+      try {
+        setProjects(await table.select({}).all());
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchProjects();
+  }, []);
+  console.log(projects);
+
+  return (
+    <div className="projectsContainer">
+      {projects.map((project) => {
+          console.log(project.fields.Site)
+        if (project.fields.Site === undefined) {
+          return (
+            <div className="project">
+              <h3>{project.fields.Name}</h3>
+              <p>{project.fields.Description}</p>
+              <div className="projectLinks">
+              <Link to={project.fields.Code}>Code</Link>
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <div className="project">
+              <h3>{project.fields.Name}</h3>
+              <p>{project.fields.Description}</p>
+              <div className="projectLinks">
+              <Link to={project.fields.Code}>Code</Link>
+              <Link to={project.fields.Site}>Site</Link>
+              </div>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default Projects;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,54 +1,22 @@
-import React, { useState, useEffect } from "react";
-import Airtable from "airtable";
-import profilePic from '../resources/profile.jpg'
+import React from "react";
+import profilePic from "../resources/profile.jpg";
+import Projects from "../components/Projects";
 
 const Home = () => {
+  return (
+    <div className="homeContainer">
+      <div className="welcomeMsg">
+        <h2>Hello, I am Sam Parsons</h2>
+        <p>Graduate Computer Scientist</p>
+        <p>
+          Solution-oriented Software Developer, with a high interest in Web
+          Technologies
+        </p>
+        <img src={profilePic} alt="Sam Parsons in there graduation Grown"></img>
+      </div>
+      <Projects />
+    </div>
+  );
+};
 
-    const [projects, setProjects] = useState([]);
-
-    useEffect(() => {
-  
-      async function fetchProjects() {
-        //   const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY }).base(
-        //       process.env.AIRTABLE_BASE_ID
-        //   );
-        var base = new Airtable({apiKey: 'keyqTF0MYSk8YUp0m'}).base('appLl8YFIJRXggdCw');
-          const table = base("Projects");
-          try {
-            setProjects( await table.select({}).all());
-          } catch (error) {
-            console.error(error);
-          }
-        }
-        fetchProjects()
-    }, []);
-    console.log(projects);
-
-    return (
-        <div className="homeContainer">
-            <div className="welcomeMsg">
-                <h2>Hello, I am Sam Parsons</h2>
-                <p>Graduate Computer Scientist</p>
-                <p>Solution-oriented Software Developer, with a high interest in Web Technologies</p>
-                <img src={profilePic} alt="Sam Parsons in there graduation Grown"></img>
-            </div> 
-
-            <div className="projects">
-                {projects.map((project) => {
-                    return (
-                        <div>
-                            <h3>{project.fields.Name}</h3>
-                            <p>{project.fields.Description}</p>
-                            <a href={project.fields.Code}>Code</a>
-                                                       
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-
-       
-    )
-}
-
-export default Home
+export default Home;

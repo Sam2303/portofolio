1a5a3ba54faaa6100d58223bb5934357719c3f7d
Sam2303 <sjp2000@live.com>
2021-09-29 13:16:28 +0100

    minor updates to styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {Link} from 'react-router-dom'
 import Airtable from "airtable";
 
 const Projects = () => {
@@ -52,7 +53,9 @@ const Projects = () => {
           );
         }
       })}
+      <Link to={"/projects"} className="moreButton">More Projects</Link>
     </div>
+      
     </>
   );
 };

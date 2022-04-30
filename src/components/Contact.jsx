c28040c429421e31a81e002d2c3a8464d782c12f
Sam2303 <sjp2000@live.com>
2022-04-30 12:13:52 +0100

    feat/update-contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Row } from 'react-bootstrap'
-import { Button } from 'bootstrap'
+import React from "react";
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
-const Contact = props => {
+const Contact = (props) => {
+
   return (
-    <div className='d-flex flex-column'>
-        <h2 className='display-2'>Contact Me</h2>
-        <a href='mailto:sjp2303@hotmail.com' className='col-md-8 row'>
-            <i class="far fa-envelope" className="display-3"></i>
-            <p>Email</p>
-        </a>
+    <div className="text-center">
+      <h2 className="display-2 text-center">Contact Me</h2>
+      <Row>
+        <Col xs={12} md={4} className="my-2 align-items-stretch d-flex">
+          <Card>
+            <Card.Body>
+              <Card.Title className="display-4">Email</Card.Title>
+              <Card.Text><i className="bi bi-mailbox display-4"></i></Card.Text>
+            </Card.Body>
+            <Button
+              variant='primary'
+              onClick={() =>  window.location.href='mailto:sjp2303@hotmail.com'}
+              className='m-1'>Email Me
+            </Button>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title className="display-4">Github</Card.Title>
+              <Card.Text><i className="bi bi-github display-4"></i></Card.Text>
+            </Card.Body>
+            <Button variant='primary'className='m-1'>
+              <a href='https://github.com/Sam2303' target='_blank' rel="noreferrer" className="text-light text-decoration-none">My Github</a>
+            </Button>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title className="display-4">Email</Card.Title>
+              <Card.Text><i className="bi bi-mailbox display-4"></i></Card.Text>
+            </Card.Body>
+            <Button
+              variant='primary'
+              onClick={() =>  window.location.href='mailto:sjp2303@hotmail.com'}
+              className='m-1'>Email Me
+            </Button>          
+          </Card>
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-Contact.propTypes = {}
+Contact.propTypes = {};
 
-export default Contact
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 // import OldApp from './OldApp';
 import PortfolioRoot from './PortfolioRoot'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
     <PortfolioRoot />, document.getElementById('root')
